27682f31a2e0d31c3d603184753d91c6fcbaf2aa
mackenziefernandez <mackenzielfernandez@gmail.com>
2016-01-06 16:40:49 -0700

    First React View developed

--- webpack.config.js
+++ webpack.config.js
@@ -46,7 +46,9 @@ const common = {
   },
   plugins: [
     new HtmlwebpackPlugin({
-      title: 'Kanban app'
+      template: 'node_modules/html-webpack-template/index.html',
+      title: 'Kanban app',
+      appMountId: 'app'
     })
   ]
 };
