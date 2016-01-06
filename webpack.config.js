239055476893ec00ee5d5e8982defca812ab820e
mackenziefernandez <mackenzielfernandez@gmail.com>
2016-01-06 12:30:30 -0700

    Splitting up configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,14 @@
 const path = require('path');
 const HtmlwebpackPlugin = require('html-webpack-plugin');
+const merge = require('webpack-merge');
 
+const TARGET = process.env.npm_lifecycle_event;
 const PATHS = {
   app: path.join(__dirname, 'app'),
   build: path.join(__dirname, 'build')
 };
 
-module.exports = {
+const common = {
   // Entry accepts a path or an object of entries.
   // The build chapter contains an example of the latter.
   entry: PATHS.app,
@@ -20,3 +22,12 @@ module.exports = {
     })
   ]
 };
+
+// Default configuration
+if(TARGET === 'start' || !TARGET) {
+  module.exports = merge(common, {});
+}
+
+if(TARGET === 'build') {
+  module.exports = merge(common, {});
+}
