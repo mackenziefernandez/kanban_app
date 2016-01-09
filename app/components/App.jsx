75659b660b57a1b86d734283ad53552b7e2e65ff
mackenziefernandez <mackenzielfernandez@gmail.com>
2016-01-08 18:00:03 -0700

    Setting up React DnD

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,12 +1,12 @@
 import AltContainer from 'alt-container';
 import React from 'react';
-// import Notes from './Notes.jsx';
-// import NoteActions from '../actions/NoteActions';
-// import NoteStore from '../stores/NoteStore';
 import Lanes from './Lanes.jsx';
 import LaneActions from '../actions/LaneActions';
 import LaneStore from '../stores/LaneStore';
+import {DragDropContext} from 'react-dnd';
+import HTML5Backend from 'react-dnd-html5-backend';
 
+@DragDropContext(HTML5Backend)
 export default class App extends React.Component {
   render() {
     return (
