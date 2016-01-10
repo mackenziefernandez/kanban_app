4dc74dfd534495b73e70bb906ddb7472d26e854f
mackenziefernandez <mackenzielfernandez@gmail.com>
2016-01-10 14:35:55 -0700

    Adding action and store method for moving

--- app/components/Notes.jsx
+++ app/components/Notes.jsx
@@ -1,14 +1,13 @@
 import React from 'react';
 import Editable from './Editable.jsx';
-
 import Note from './Note.jsx';
+import LaneActions from '../actions/LaneActions';
 
 export default ({notes, onValueClick, onEdit, onDelete}) => {
   return (
     <ul className="notes">{notes.map((note) =>
       <Note className="note" key={note.id} id={note.id}
-        onMove={({sourceId, targetId}) =>
-        console.log('source', sourceId, 'target', targetId)}>
+        onMove={LaneActions.move} >
         <Editable editing={note.editing} value={note.task}
           onValueClick={onValueClick.bind(null, note.id)}
           onEdit={onEdit.bind(null, note.id)}
