1aa829019d5babf9d0c3a67451196a46f209bd8e
mackenziefernandez <mackenzielfernandez@gmail.com>
2016-01-10 13:58:16 -0700

    Hookups for the drag and drop of a note

--- app/components/Notes.jsx
+++ app/components/Notes.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import Editable from './Editable.jsx';
 
+import Note from './Note.jsx';
+
 export default ({notes, onValueClick, onEdit, onDelete}) => {
   return (
     <ul className="notes">{notes.map((note) =>
-      <li className="note" key={note.id}>
+      <Note className="note" key={note.id} id={note.id}>
         <Editable editing={note.editing} value={note.task}
           onValueClick={onValueClick.bind(null, note.id)}
           onEdit={onEdit.bind(null, note.id)}
           onDelete={onDelete.bind(null, note.id)} />
-      </li>
+      </Note>
     )}</ul>
   );
 }
