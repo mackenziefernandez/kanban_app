74fa3833f0d17f75a453c920aa0acae06a2d0aae
mackenziefernandez <mackenzielfernandez@gmail.com>
2016-01-10 14:48:21 -0700

    Implementing Note drag and drop logic

--- app/components/Note.jsx
+++ app/components/Note.jsx
@@ -8,6 +8,9 @@ const noteSource = {
     return {
       id: props.id
     };
+  },
+  isDragging(props, monitor) {
+    return props.id === monitor.getItem().id;
   }
 };
 
@@ -24,17 +27,18 @@ const noteTarget = {
   }
 };
 
-@DragSource(ItemTypes.NOTE, noteSource, (connect) => ({
-  connectDragSource: connect.dragSource()
+@DragSource(ItemTypes.NOTE, noteSource, (connect, monitor) => ({
+  connectDragSource: connect.dragSource(),
+  isDragging: monitor.isDragging() // map isDragging() state to isDragging prop
 }))
 @DropTarget(ItemTypes.NOTE, noteTarget, (connect) => ({
   connectDropTarget: connect.dropTarget()
 }))
 export default class Note extends React.Component {
   render() {
-    const {connectDragSource, connectDropTarget, id, onMove, ...props} = this.props;
+    const {connectDragSource, connectDropTarget, isDragging, id, onMove, ...props} = this.props;
     return connectDragSource(connectDropTarget(
-      <li {...props}>{props.children}</li>
+      <li style={{ opacity: isDragging ? 0 : 1 }} {...props}>{props.children}</li>
     ));
   }
 }
